4930b0a4ddf30955dc6ba8a157334d70476374a5
corralien <damien.corral@gmail.com>
2022-11-08 21:22:33 +0100

    create a model factory

---
+++ yawbcc/models.py
@@ -0,0 +1,36 @@
+from tensorflow.keras.layers import Input, Lambda, Flatten, Dense, Dropout, Conv2D, GlobalAveragePooling2D
+from tensorflow.keras.layers import RandomRotation, RandomFlip
+from tensorflow.keras.models import Model, Sequential
+import tensorflow as tf
+import inspect
+import pathlib
+
+
+def model_factory(base_model, top_net, input_shape=(128, 128, 3), pooling=None, transformers=None, name=None):
+    # Try to locate a trained model based on its name
+    model_function = getattr(tf.keras.applications, base_model)
+    module_name = pathlib.Path(inspect.getfile(model_function)).stem
+    model_module = getattr(tf.keras.applications, module_name)
+
+    # Load base model and freeze layers
+    app_model = model_function(weights='imagenet', input_shape=input_shape,
+                               include_top=False, pooling=pooling)
+
+    # Input layer
+    inputs = Input(shape=input_shape, name='input')
+
+    # Image transformers
+    transformers = [] if transformers is None else transformers
+    x = functools.reduce(lambda x, f: f(x), transformers, inputs)
+
+    # Base model preprocessing
+    x = Lambda(model_module.preprocess_input, name='preprocess')(x)
+
+    # Build model
+    x = app_model(x, training=True)
+
+    # Top net stack
+    x = functools.reduce(lambda x, f: f(x), top_net, x)
+
+    return Model(inputs, x, name=name)
+
