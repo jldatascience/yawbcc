633ecf47368263aab64f8bc4cd885f80d236376d
corralien <damien.corral@gmail.com>
2022-11-10 16:20:20 +0100

    fix some bugs

--- yawbcc/datasets.py
+++ yawbcc/datasets.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 import requests
 from PIL import Image
--- yawbcc/models.py
+++ yawbcc/models.py
@@ -1,20 +1,21 @@
-from tensorflow.keras.layers import Input, Lambda, Flatten, Dense, Dropout, Conv2D, GlobalAveragePooling2D
-from tensorflow.keras.layers import RandomRotation, RandomFlip
-from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Input, Lambda
+from tensorflow.keras.models import Model
 import tensorflow as tf
 import inspect
 import pathlib
+import functools
 
 
-def model_factory(base_model, top_net, input_shape=(128, 128, 3), pooling=None, transformers=None, name=None):
+def model_factory(app_name, top_net, input_shape=(128, 128, 3), pooling=None, transformers=None, name=None):
     # Try to locate a trained model based on its name
-    model_function = getattr(tf.keras.applications, base_model)
+    model_function = getattr(tf.keras.applications, app_name)
     module_name = pathlib.Path(inspect.getfile(model_function)).stem
     model_module = getattr(tf.keras.applications, module_name)
 
     # Load base model and freeze layers
     app_model = model_function(weights='imagenet', input_shape=input_shape,
                                include_top=False, pooling=pooling)
+    app_model.trainable = False
 
     # Input layer
     inputs = Input(shape=input_shape, name='input')
@@ -27,7 +28,7 @@ def model_factory(base_model, top_net, input_shape=(128, 128, 3), pooling=None, 
     x = Lambda(model_module.preprocess_input, name='preprocess')(x)
 
     # Build model
-    x = app_model(x, training=True)
+    x = app_model(x, training=False)
 
     # Top net stack
     x = functools.reduce(lambda x, f: f(x), top_net, x)
